9fd713c3423b130de43acd32e9aafff50d5a6001
Saurin Shah <saurin.shah94@gmail.com>
2018-06-05 09:05:19 -0400

    Trying to add comment box

--- stuboverflow/model/question/question.schema.server.js
+++ stuboverflow/model/question/question.schema.server.js
@@ -6,7 +6,8 @@ var postSchema = mongoose.Schema({
     viewsCount: String,
     answersCount: String,
     _creator: {type: mongoose.Schema.Types.ObjectId, ref:'UserModel'},
-    dateCreated: {type: Date, default: Date.now}
+    dateCreated: {type: Date, default: Date.now},
+    comments: [String]
 }, {collection: "question"});
 
 module.exports = postSchema;
